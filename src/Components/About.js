693d4b62ea54f9ab9a3cc641aa39e070c2c7d1aa
Felix Ondullah <felixondullah@gmail.com>
2023-01-06 18:15:28 +0300

    added about us content

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,30 +1,43 @@
-import { Container, Row, Col,Image} from 'react-bootstrap'
-import React from 'react'
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import React from "react";
+import image from "../Assets/back.jpg";
 
 function About() {
   return (
     <div>
-    <main>
-      <Container>
-        <Row className='px-4 my-5'>
-          <Col sm={7}>
-            <Image
-             src ="https://unsplash.com/photos/LvySG1hvuzI"
-             fluid
-             rounded
-            />
-          
-          </Col>
-          <Col sm={5}>sm-4</Col>
-
-        </Row>
-      </Container>
-    </main>
-  
+      <main>
+        <Container>
+          <Row className="px-4 my-5">
+            <Col sm={7}>
+              <Image
+                class="thumbnail img-responsive"
+                src={image}
+                alt="home"
+                height="400"
+                width="500"
+                fluid
+                rounded
+                className=""
+              />
+            </Col>
+            <Col sm={5}>
+              <h1 class="font-weigh-light">What We Do</h1>
+              <p class="mt-4 " style={{fontSize:"17px"}} >
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+              <Button variant= "outline-primary"> Try Us</Button>
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </div>
-
-    
-  )
+  );
 }
 
-export default About
+export default About;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import image from "../Assets/cart3.jpg";
 
 function Home() {
-  return <h1>Hompage</h1>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${image})`,
+        position: "fixed",
+        backgroundSize: "cover",
+        height: "100vh",
+        backgroundRepeat: "no-repeat",
+        width: "215vh",
+      }}
+    >
+      <div>
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
