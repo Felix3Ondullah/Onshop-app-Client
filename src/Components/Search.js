8715856dbc8919509140232882123d158b50729b
Felix Ondullah <felixondullah@gmail.com>
2023-01-11 12:54:02 +0300

    implemented useffect

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import {useState, useEffect}from 'react'
 
 function Search() {
+  const[productSearch, setProductSearch] = useState()
+  useEffect (() => {
+  fetch('http://localhost:3005/products')
+  .then(response => response.json ())
+  .catch(error => console.log(error))
+  }, [])
+
+
   return (
-    <h2>Search page</h2>
+    <div>Search Products</div>
   )
 }
 
