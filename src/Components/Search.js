295741eeb618ccb1cec6dbf3f799f5085c58d538
Felix Ondullah <felixondullah@gmail.com>
2023-01-11 14:24:44 +0300

    added bootsrap product table

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,37 +1,42 @@
 import { useState, useEffect } from "react";
 
 function Search() {
-  const [ProductList,setProductList] = useState();
+  const [ProductList, setProductList] = useState();
+
   useEffect(() => {
-    fetch("http://localhost:3005/products")
+    fetch("http://localhost:3004/products")
       .then((response) => response.json())
-      .then(result=>setProductList(result))
+      .then((result) => setProductList(result))
       .catch((error) => console.log(error));
   }, []);
 
-  return <div>
-    <table>
-      <tr>
-        <th>Image</th>
-        <th>Product</th>
-        <th>Price</th>
-        <th>Rating</th>
-      </tr>
-      {
-        ProductList && ProductList.length> 0 ?
-        ProductList.map(product =>
-    <tr>
-      <td>{product.img}</td>
-      <td>{product.product}</td>
-      <td>{product.price}</td>
-      <td>{product.rating}</td>
-    </tr>
-    )
-      :'Loading'
-      }
-    </table>
+  return (
+    <div>
+      <table class="table table-bordered">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col"> Image</th>
+            <th scope="col">Product</th>
+            <th scope="col"> Price</th>
+            <th scope="col"> Rating</th>
+          </tr>
+        </thead>
 
-  </div>;
+        {ProductList && ProductList.length > 0
+          ? ProductList.map((product) => (
+              <tbody>
+                <tr>
+                  <td>{product.img}</td>
+                  <td>{product.product}</td>
+                  <td>{product.price}</td>
+                  <td>{product.rating}</td>
+                </tr>
+              </tbody>
+            ))
+          : "Loading"}
+      </table>
+    </div>
+  );
 }
 
 export default Search;
