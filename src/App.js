877da86425590b1427a1518b7deff66d2e51e5fa
Allan Kimurgor <allan.kimurgor@student.moringaschool.com>
2023-01-13 02:00:34 +0300

    Added ForgotPassword component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Footer from "./Components/Footer";
 import Search from "./Components/Search";
+// import ForgotPassword from "./Components/ForgotPassword";
+import LandingPage from "./Components/LandingPage";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
 
 function App() {
@@ -16,7 +20,10 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/search" element={<Search />} />
-        {/* <Route path="/login" component={Login} /> */}
+        {/* <Route path="/forgotPassword" element={<ForgotPassword/>} /> */}
+        <Route path="/landingPage" element={<LandingPage/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
       <Footer/>
     </Router>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../Css/Footer.css";
 
+
 const Footer = () => {
   return (
     <div className="copyright">
