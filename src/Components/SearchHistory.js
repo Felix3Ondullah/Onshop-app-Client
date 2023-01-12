49f3efa9096a11ac70a14d71a80cb5956d910132
Felix Ondullah <felixondullah@gmail.com>
2023-01-12 13:15:12 +0300

    implemented search hitsory page

---
+++ src/Components/SearchHistory.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Table, Input, Button } from 'antd';
+
+const SearchHistory = () => {
+  const [searchHistory, setSearchHistory] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('https://endpoint/user/search-history')
+      .then(res => res.json())
+      .then(data => setSearchHistory(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  const handleSearch = () => {
+    
+  }
+
+  const columns = [
+    {
+      title: 'Product Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Search Date',
+      dataIndex: 'searchDate',
+      key: 'searchDate',
+    },
+  ];
+
+  return (
+    <div>
+      <Input placeholder="Search for a specific term" onChange={e => setSearchTerm(e.target.value)} />
+      <Button onClick={handleSearch}>Search</Button>
+      <Table dataSource={searchHistory} columns={columns} />
+    </div>
+  );
+}
+
+export default SearchHistory;
