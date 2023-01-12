6cbcb862b2e56c63bec5f6e7ba04477e1c733c66
Allan Kimurgor <allan.kimurgor@student.moringaschool.com>
2023-01-13 02:01:48 +0300

    Added LandingPage

--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -1,26 +1,26 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-// import "../Css/LoginRegister.css"
+import "../Css/LoginRegister.css"
 
-// export default function ForgotPasswordPage() {
-//     return (
-//         <div className="text-center m-5-auto">
-//             <h2>Reset your password</h2>
-//             <h5>Enter your email address and we will send you a link to reset your password.</h5>
-//             <form action="/login">
-//                 <p>
-//                     <label id="reset_pass_lbl">Email address</label><br/>
-//                     <input type="email" name="email" required />
-//                 </p>
-//                 <p>
-//                     <button id="sub_btn" type="submit">Send password reset email</button>
-//                 </p>
-//             </form>
-//             <footer>
-//                 <p>First time? <Link to="/register">Create an account</Link>.</p>
-//                 <p><Link to="/">Back to Homepage</Link>.</p>
-//             </footer>
-//         </div>
-//     )
-// }
+export default function ForgotPasswordPage() {
+    return (
+        <div className="text-center m-5-auto">
+            <h2>Reset your password</h2>
+            <h5>Enter your email address and we will send you a link to reset your password.</h5>
+            <form action="/login">
+                <p>
+                    <label id="reset_pass_lbl">Email address</label><br/>
+                    <input type="email" name="email" required />
+                </p>
+                <p>
+                    <button id="sub_btn" type="submit">Send password reset email</button>
+                </p>
+            </form>
+            <footer>
+                <p>First time? <Link to="/register">Create an account</Link>.</p>
+                <p><Link to="/">Back to Homepage</Link>.</p>
+            </footer>
+        </div>
+    )
+}
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
 
-import "../Css/LoginRegister.css"
+// import "../Css/LoginRegister.css"
 
-export default function SignUpPage() {
+// export default function SignUpPage() {
 
-    return (
-        <div className="reg-cont">
-            <h2>Join us</h2>
-            <h5>Create your personal account</h5>
-            <form action="/home">
-                <p>
-                    <label>Username</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Email address</label><br/>
-                    <input type="email" name="email" required />
-                </p>
-                <p>
-                    <label>Password</label><br/>
-                    <input type="password" name="password" required />
-                </p>
+//     return (
+//         <div className="reg-cont">
+//             <h2>Join us</h2>
+//             <h5>Create your personal account</h5>
+//             <form action="/home">
+//                 <p>
+//                     <label>Username</label><br/>
+//                     <input type="text" name="first_name" required />
+//                 </p>
+//                 <p>
+//                     <label>Email address</label><br/>
+//                     <input type="email" name="email" required />
+//                 </p>
+//                 <p>
+//                     <label>Password</label><br/>
+//                     <input type="password" name="password" required />
+//                 </p>
         
-                <p>
-                    <button id="sub_btn" type="submit">Register</button>
-                </p>
-            </form>
-            <footer>
-                <p >
-                <Link to="/" className='foot-cont'>Back to Homepage</Link>.
-                </p>
-            </footer>
-        </div>
-    )
+//                 <p>
+//                     <button id="sub_btn" type="submit">Register</button>
+//                 </p>
+//             </form>
+//             <footer>
+//                 <p >
+//                 <Link to="/" className='foot-cont'>Back to Homepage</Link>.
+//                 </p>
+//             </footer>
+//         </div>
+//     )
 
-}
+// }
